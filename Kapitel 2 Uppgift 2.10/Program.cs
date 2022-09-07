dcf6b9cc15a18be9413424ddfd578873c2f287ff
EmilWallett <nhlpool27@gmail.com>
2022-09-07 12:08:01 +0200

    Add project files.

---
+++ Kapitel 2 Uppgift 2.10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Upgift_2._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din favorite book?");
+            string HarryPotter = Console.ReadLine();
+            Console.WriteLine("Villken bra book");
+        }
+        
+    }
+}
